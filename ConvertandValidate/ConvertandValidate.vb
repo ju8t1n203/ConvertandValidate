a2031c36d950cc3fc8b01c6baa2faea99e55f177
Justin Bell <Juxin203@gmail.com>
2024-09-22 20:55:51 -0600

    Add project files.

---
+++ ConvertandValidate/ConvertandValidate.vb
@@ -0,0 +1,59 @@
+﻿Option Compare Text
+Option Explicit On
+Option Strict On
+'Justin Bell
+'RCET0265
+'Fall 2024
+'Convert and Validate
+' link
+Imports System.Threading
+
+Module ConvertandValidate
+
+    Sub Main()
+
+        'just the given code
+        Dim aValidNumber As Integer
+        Dim usernResponse As String
+        'defined the usernResponse varible
+
+        Do
+            Console.WriteLine("press ""Q"" to quit.")
+            Console.WriteLine($"Enter a number:")
+            usernResponse = Console.ReadLine()
+
+            If usernResponse = "q" Then
+                Exit Do
+            End If
+
+            If ConversionValid(usernResponse, aValidNumber) = True Then
+                Console.WriteLine($"{usernResponse} converted successfully to {aValidNumber}!")
+            Else
+                Console.WriteLine($"Oops, {usernResponse} does not seem to be a number")
+            End If
+
+            'clears the console after one second, for fun
+            Thread.Sleep(1000)
+            Console.Clear()
+
+        Loop
+
+    End Sub
+
+    'it works
+
+    Function ConversionValid(convertThisString As String, ByRef toThisInteger As Integer) As Boolean
+        'this produces a status to be returned
+        Dim status As Boolean = False
+
+        Try
+            toThisInteger = CInt(convertThisString)
+            status = True
+        Catch ex As Exception
+            status = False
+        End Try
+
+        Return status
+    End Function
+
+End Module
---
+++ ConvertandValidate/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ConvertandValidate")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ConvertandValidate")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ee83a54d-99cc-41bc-8658-81f869cb75ab")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
